6444af11df532f913fb167f3930b0c5c07fb6a1c
SoumabhaSaha15 <soumabhasaha1509+github@gmail.com>
2025-12-10 00:37:14 +0530

    prod setting fixed.

--- src/utility/AxiosBase.ts
+++ src/utility/AxiosBase.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
+const baseURL = import.meta.env.DEV ? "/api" : (import.meta.env.VITE_OW_URL ?? "/");
+// dev proxy from [vite.config.ts](vite.config.ts)
 export default axios.create({
-  baseURL: "/api",
+  baseURL,
   validateStatus: (status) => !(status >= 500)
 });
