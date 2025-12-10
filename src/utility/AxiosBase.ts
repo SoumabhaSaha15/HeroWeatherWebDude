b3e7c049eede1a06b55506ddbc6027b7e79be89c
SoumabhaSaha15 <soumabhasaha1509+github@gmail.com>
2025-12-10 13:00:29 +0530

    vercel.json added

--- src/utility/AxiosBase.ts
+++ src/utility/AxiosBase.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-const baseURL = import.meta.env.DEV ? "/api" : (import.meta.env.VITE_OW_URL ?? "/");
+// const baseURL = import.meta.env.DEV ? "/api" : (import.meta.env.VITE_OW_URL ?? "/");
 // dev proxy from [vite.config.ts](vite.config.ts)
 export default axios.create({
-  baseURL,
+  baseURL:"/api",
   validateStatus: (status) => !(status >= 500)
 });
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { defineConfig, loadEnv } from "vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), 'VITE_');
   const targetUrl = env.VITE_OW_URL;
-  console.log(targetUrl);
   return {
     plugins: [
       react(),
